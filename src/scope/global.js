487749426f4355961a6adfbaeaf2f8d36af2dbc5
pablosc <pspablosanchezc@gmail.com>
2021-07-21 16:12:10 -0500

    creación de tres proyectos

---
+++ src/scope/global.js
@@ -0,0 +1,11 @@
+var Hello = 'Hello';
+let world = 'Hello world';
+const HelloWorld = 'Hello World!';
+
+const anotherFunction = () => {
+    console.log(Hello);
+    console.log(world);
+    console.log(HelloWorld);
+}
+
+anotherFunction();
